5e8d20763716dd59882c74e826ed36748cfa4019
Nicolas Combaret <nicolas.combaret@gmail.com>
2015-09-03 22:21:35 +0200

    projet-musique: Déplacement de la définition du constructeur dans playlistmanager.cpp

--- projet-musique/core/playlistmanager.cpp
+++ projet-musique/core/playlistmanager.cpp
@@ -3,14 +3,14 @@
 
 
 
-PlaylistManager::~PlaylistManager()
+PlaylistManager::PlaylistManager() :
+    QObject()
+  , m_playlist( new QMediaPlaylist )
 {
 }
 
 
 
-PlaylistManager::PlaylistManager() :
-    QObject()
-  , m_playlist( new QMediaPlaylist )
+PlaylistManager::~PlaylistManager()
 {
 }
