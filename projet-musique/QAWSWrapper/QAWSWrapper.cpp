3ace21818c28f8f2f3d28d37bd72f10f7457b105
Nicolas Combaret <nicolas.combaret@gmail.com>
2016-03-03 22:56:59 +0100

    QAWSWrapper: [Refact] Rename getTimeStamp() to getCurrentTimeStamp()

--- projet-musique/QAWSWrapper/QAWSWrapper.cpp
+++ projet-musique/QAWSWrapper/QAWSWrapper.cpp
@@ -65,7 +65,7 @@ namespace
     /**
      * @brief Return current time stamp with the ISO formatting
      */
-    QString getTimeStamp()
+    QString getCurrentTimeStamp()
     {
         return QDateTime::currentDateTimeUtc().toString( Qt::ISODate );
     }
@@ -217,7 +217,7 @@ AlbumPhys QAWSWrapper::getAlbumFromEAN( const QString& ean )
     listOfParameters.addQueryItem( "ResponseGroup", QString( "Tracks,Small,Images,ItemAttributes" ).toUtf8().toPercentEncoding() );
     listOfParameters.addQueryItem( "SearchIndex", "Music" );
     listOfParameters.addQueryItem( "Service", "AWSECommerceService" );
-    listOfParameters.addQueryItem( "Timestamp", getTimeStamp().toUtf8().toPercentEncoding() );
+    listOfParameters.addQueryItem( "Timestamp", getCurrentTimeStamp().toUtf8().toPercentEncoding() );
     //qDebug() << listOfParameters.query();
 
     // Prepare the string that will be used to compute the signature
