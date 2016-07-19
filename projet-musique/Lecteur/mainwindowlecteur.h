647becf29b0d89bc8eaa64b5d830d3fc139adc75
Alex <alexandra.dagallier@gmail.com>
2016-07-19 19:51:11 +0200

    fixup! Renommage de MainWindow en MainWindowLecteur

--- projet-musique/Lecteur/mainwindowlecteur.h
+++ projet-musique/Lecteur/mainwindowlecteur.h
@@ -1,5 +1,5 @@
-#ifndef MAINWINDOW_H
-#define MAINWINDOW_H
+#ifndef MAINWINDOWLECTEUR_H
+#define MAINWINDOWLECTEUR_H
 
 #include <QWidget>
 #include "dialogcontroles.h"
@@ -27,4 +27,4 @@ private:
 
 };
 
-#endif // MAINWINDOW_H
+#endif // MAINWINDOWLECTEUR_H
