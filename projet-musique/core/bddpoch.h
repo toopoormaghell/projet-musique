9cdc2d02f8aeeef18f37a81c99aa41e59d10abb8
Nicolas Combaret <nicolas.combaret@gmail.com>
2017-10-01 10:56:14 +0200

    Homogeneize blank characters appearence

--- projet-musique/core/bddpoch.h
+++ projet-musique/core/bddpoch.h
@@ -16,16 +16,16 @@ public:
 
     void updateBDD();
     void supprimerenBDD() const;
-    static BDDPoch* recupererBDD( const int id );
+    static BDDPoch* recupererBDD(const int id);
 
     QImage m_image;
     QString m_chemin;
 
-    static QString creerchemin( const QString& album, const QString& artiste );
-    static QList<int> pochettesparart( const QString& artiste );
+    static QString creerchemin(const QString& album, const QString& artiste);
+    static QList<int> pochettesparart(const QString& artiste);
 
 private:
-    void sauverImage( const QString& album, const QString& artiste );
+    void sauverImage(const QString& album, const QString& artiste);
     void recupererId();
     void ajouterBDD();
 
